7143c8ee0b9941ff25aa3f09b7cdea9344a11d18
ShadabImran07 <shadabimran07@gmail.com>
2024-06-09 14:53:05 +0530

    docker vloume error solve

--- frontend/lib/actions/fetData.ts
+++ frontend/lib/actions/fetData.ts
@@ -4,10 +4,15 @@ import axios from "axios";
 const api_key: string = "12345";
 
 export const fetchData = async () => {
-	const response = await axios.get("http://localhost:8080/api/price/list", {
-		headers: {
-			"x-api-key": `${api_key}`,
-		},
-	});
-	return response.data;
+	try {
+		const response = await axios.get("http://localhost:8080/api/price/list", {
+			headers: {
+				"x-api-key": `${api_key}`,
+			},
+		});
+		return response.data;
+	} catch (error) {
+		console.log(error);
+		return null;
+	}
 };
